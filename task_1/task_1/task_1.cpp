9e80ab7e730c9f55a5109c710f7ce479c4faed18
Vasyl Lendo <vLendyo@devvision.org>
2023-03-02 12:02:53 +0200

    edit

---
+++ task_1/task_1/task_1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main() {
+	int a, sum = 0;
+	cout << "Enter the value of a: ";
+	cin >> a;
+	for (int i = a; i <= 500; i++) {
+		sum += i;
+	}
+	cout << "The sum integers from " << a << " to 500: " << sum << endl;
+}
